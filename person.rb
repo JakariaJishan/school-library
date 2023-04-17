b2d514bb714510efeb4e2505cc7eb1868dcbd3ed
JakariaJishan <jakariajishan987@gmail.com>
2023-04-18 02:53:56 +0600

    Create class person and add methods

---
+++ person.rb
@@ -0,0 +1,32 @@
+class Person 
+
+    attr_reader :id
+    attr_accessor :name
+    attr_accessor :age
+
+    def initialize(name = "Unknown",age, parent_permission = true )
+        @id = Random.rand(1..1000)
+        @name=name
+        @parent_permission = parent_permission
+        @age=age
+    end
+
+    def can_use_services? 
+        if @age >= 18 || @parent_permission
+            return true
+         else 
+             return false
+         end
+    end
+
+    private
+
+    def of_age? 
+        if @age >= 18 
+           return true
+        else 
+            return false
+        end
+    end
+
+end

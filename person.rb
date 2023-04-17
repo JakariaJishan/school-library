dbdbfe48f317b3a86ea65621700b00a63991bb7d
JakariaJishan <jakariajishan987@gmail.com>
2023-04-18 02:57:04 +0600

    Create class student that extends from person.

--- person.rb
+++ person.rb
@@ -30,3 +30,15 @@ class Person
     end
 
 end
+
+
+class Student < Person
+    def initialize(classroom)
+        super(name = "Unknown",age, parent_permission = true)
+        @classroom = classroom
+    end
+
+    def play_hooky
+        "¯\(ツ)/¯"
+    end
+end

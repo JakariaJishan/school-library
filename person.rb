aa255818bb4c5632ca93ec45a2ab3c53b9551394
JakariaJishan <jakariajishan987@gmail.com>
2023-04-18 02:59:05 +0600

    Create class teacher that extends from person

--- person.rb
+++ person.rb
@@ -42,3 +42,14 @@ class Student < Person
         "¯\(ツ)/¯"
     end
 end
+
+class Teacher < Person 
+    def initialize(specialization)
+        super(name = "Unknown",age, parent_permission = true)
+        @specialization = specialization
+    end
+
+    def can_use_services? 
+        return true
+    end
+end

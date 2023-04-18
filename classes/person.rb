c343d5b515e91c0834d9c32e31bce387e45a04a8
JakariaJishan <jakariajishan987@gmail.com>
2023-04-18 21:36:46 +0600

    Fix rubocop errors

--- classes/person.rb
+++ classes/person.rb
@@ -1,32 +1,28 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
 
-class Person 
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @parent_permission = parent_permission
+    @age = age
+  end
 
-    attr_reader :id
-    attr_accessor :name, :age
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    def initialize(age,name = "Unknown", parent_permission: true )
-        @id = Random.rand(1..1000)
-        @name=name
-        @parent_permission = parent_permission
-        @age=age
-    end
+  def read_name
+    @name
+  end
 
-    def can_use_services? 
-        of_age?|| @parent_permission
-    end
+  private
 
-    def read_name
-        @name
-    end
-    private
+  def of_age?
+    return true if @age >= 18
 
-    def of_age? 
-        if @age >= 18 
-           return true
-        else 
-            return false
-        end
-    end
 
+    false
+  end
 end
-

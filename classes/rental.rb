9db14eecb61f7a3fa4ecbf4c6fb8e43c92bb2cd2
JakariaJishan <jakariajishan987@gmail.com>
2023-04-21 01:00:05 +0600

    Create the many-to-many (also has-many-through) relationship between Person and Book using the intermediate class Rental.

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,6 +1,10 @@
 class Rental
-    attr_accessor :date
-    def initialize(date)
+    attr_accessor :date, :book, :person
+    def initialize(date, book, person)
         @date=date
+        @book = book
+        book.rentals << self
+        @person = person
+        person.rentals << self
     end
 end

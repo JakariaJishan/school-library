aa70f9f3e24f9f53832b8af02c054e6b24dbb0af
JakariaJishan <jakariajishan987@gmail.com>
2023-04-21 01:01:13 +0600

    Fix rubocop errors

--- classes/book.rb
+++ classes/book.rb
@@ -1,10 +1,10 @@
 class Book
-    attr_accessor :title, :author
-    attr_reader :rentals
+  attr_accessor :title, :author
+  attr_reader :rentals
 
-    def initialize(title, author)
-        @title = title
-        @author = author
-        @rentals = []
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 end
--- classes/classroom.rb
+++ classes/classroom.rb
@@ -1,14 +1,14 @@
-class Classroom 
-    attr_accessor :label
-    attr_reader :students
-    def initialize(label)
-        @label=label
-        @students = []
-    end
+class Classroom
+  attr_accessor :label
+  attr_reader :students
 
-    def add_student(student)
-        @students << student
-        student.classroom = self
-    end
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
 end
